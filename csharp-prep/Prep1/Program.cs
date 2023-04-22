a76b0db827cee366bfd4b165386383421647aa8f
erickseidmann <erickseidmann@gmail.com>
2023-04-21 21:02:14 -0300

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //tips never never push debug anaway that is going to use your old
+        Console.Write("What is your First(1) name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        
+        Console.WriteLine($"So your name is {last}, {first} {last}");
     }
 }
