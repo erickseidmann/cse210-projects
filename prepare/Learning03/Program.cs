81a91d86df24a36e2c3f1225ea29746dea18aba2
erickseidmann <erickseidmann@gmail.com>
2023-05-17 22:28:49 -0300

    I finished learning 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction calculate_fraction1 = new Fraction(11, 12);
+        Console.WriteLine(calculate_fraction1.GetFractionString());
+        Console.WriteLine(calculate_fraction1.GetDecimalValue());
+
+        Fraction calculate_fraction2 = new Fraction(18);
+        Console.WriteLine(calculate_fraction2.GetFractionString());
+        Console.WriteLine(calculate_fraction2.GetDecimalValue());
+
+        Fraction calculate_fraction3 = new Fraction(9, 3);
+        Console.WriteLine(calculate_fraction3.GetFractionString());
+        Console.WriteLine(calculate_fraction3.GetDecimalValue());
+
     }
 }
