3385aaaa23910a21146b7fcc4330449e9824850f
erickseidmann <erickseidmann@gmail.com>
2023-05-26 07:41:23 -0300

    Key words solved

---
+++ prove/Develop03/KeyWords.cs
@@ -0,0 +1,13 @@
+class keyWords
+{
+    private String Keyword_1 {get;}
+
+    public keyWords(string keyWords)
+    {
+        Keyword_1 = keyWords;
+    }
+    public String returningKeyword_1()
+    {
+        return Keyword_1;
+    }
+}
